4413b86847a434828aa3de3e19bf454304b01716
Darya Vanichkina <darya.vanichkina@sydney.edu.au>
2021-08-13 10:49:58 +1000

    Added date export code. Cleaned some extra characters as per client request.

--- src/002_prepare_corpus.py
+++ src/002_prepare_corpus.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from utils import get_project_root
 from functs import get_byline, parse_filename, readfilesin
-from functs import make_slug, get_text4digitcode, clean_nonascii, clean_quotes
+from functs import get_text4digitcode, clean_nonascii, clean_quotes, get_date, clean_quot, replace_six_questionmarks
+from datetime import datetime
+
 # imports for debugging non-unicode characters
 # from functs import find_problems, find_filename_from_string, find_specific_character_with_preceding, 
 # from functs import print_body_from_string, find_specific_character_wout_preceding, 
 # from functs import where_is_byline
 
 
-
-projectroot = str(get_project_root()) 
+projectroot = str(get_project_root())
 rawdatapath = projectroot + "/100_data_raw/Australian Obesity Corpus/"
 processeddatapath = projectroot + "/300_data_processed/"
 
@@ -25,30 +26,30 @@ filesdf['contents'] = result
 
 # filter out those files that don't have the word "body" (case-insensitive) in them
 # These are the list files and the zip file
-filesdf = filesdf[filesdf['contents'].str.contains('body', case = False)]
+filesdf = filesdf[filesdf['contents'].str.contains('body', case=False)]
 
 # split header and body, thankfully case-sensitive works
-filesdf['contents_split'] =  filesdf['contents'].str.split("\nBody\n")
+filesdf['contents_split'] = filesdf['contents'].str.split("\nBody\n")
 # always splits into two
 # filesdf['contents_len'] = filesdf['contents_split'].str.len()
 # take advantage of this:
-filesdf[['metadata','body']] = pd.DataFrame(filesdf.contents_split.tolist(), index= filesdf.index)
+filesdf[['metadata', 'body']] = pd.DataFrame(filesdf.contents_split.tolist(), index=filesdf.index)
 filesdf = filesdf.drop(columns=['contents_split', 'contents'])
 
 # grab the best pass at the title, and convert all to title case
-filesdf = filesdf.assign(title = filesdf['metadata'].map(lambda x: x.partition('\n')[0].title()))
+filesdf = filesdf.assign(title=filesdf['metadata'].map(lambda x: x.partition('\n')[0].title()))
 
 # get date/month from the relevant directory of the filepath
 filename_fields = filesdf["filename"].apply(parse_filename)
 filesdf = filesdf.assign(**filename_fields)
 # extract the four digit code from the filename
-filesdf = filesdf.assign(fourdigitcode = filesdf['filename'].map(get_text4digitcode))
+filesdf = filesdf.assign(fourdigitcode=filesdf['filename'].map(get_text4digitcode))
 
 # where are the bylines?
 # code used for eda but not needed later
-#filesdf = filesdf.assign(byline_locs = filesdf['contents_split'].map(lambda x: where_is_byline(x)))
-#filesdf['bylines_flat'] = [','.join(map(str, l)) for l in filesdf['byline_locs']]
-filesdf = filesdf.assign(byline = filesdf['metadata'].map(lambda x: get_byline(x)))
+# filesdf = filesdf.assign(byline_locs = filesdf['contents_split'].map(lambda x: where_is_byline(x)))
+# filesdf['bylines_flat'] = [','.join(map(str, l)) for l in filesdf['byline_locs']]
+filesdf = filesdf.assign(byline=filesdf['metadata'].map(lambda x: get_byline(x)))
 # lots of room to clean bylines if needed/desired...
 
 
@@ -63,6 +64,25 @@ filesdf["title"] = filesdf["title"].apply(clean_nonascii)
 # clean up quotes
 filesdf["body"] = filesdf["body"].apply(clean_quotes)
 filesdf["title"] = filesdf["title"].apply(clean_quotes)
+# and the random repeats of "quot;"
+filesdf["title"] = filesdf["title"].apply(clean_quot)
+filesdf["body"] = filesdf["body"].apply(clean_quot)
+# and replace six ?????? specifically with double quotes...
+filesdf["body"] = filesdf["body"].apply(replace_six_questionmarks)
+
+# get dates
+filesdf['date'] = filesdf["metadata"].apply(get_date)
+# some may not parse
+print("The total number of files where we were unable to find a date in the metadata was ", sum(filesdf.date.isna()),
+      "files.")
+print("These will be replaced from the filename, setting the date to the 1st of the month.")
+# if unable to be detected replace with the first of the month and year that exists in the filename
+# at least for this corpus - 1 file, no date in raw file
+filesdf['tmpdate'] = filesdf.apply(
+    lambda x: (datetime.strptime(str(x['year'] + "-" + x['numeric_month'] + "-01 00:00:00"), "%Y-%m-%d %H:%M:%S")),
+    axis=1)
+filesdf['date'] = filesdf['date'].fillna(filesdf['tmpdate'])
+filesdf.drop('tmpdate', axis=1, inplace=True)
 
 # can use the below functions to narrow down issues ---
 # find_problems(start, end, colname = "cleaned_bodies", filesdf = filesdf)
@@ -72,29 +92,5 @@ filesdf["title"] = filesdf["title"].apply(clean_quotes)
 # print_body_from_string(string, filesdf = filesdf)
 
 # write corpus out as per client's request
-filesdf['outputfilename'] =  filesdf.apply(lambda x: \
-    f"../200_data_clean/corpus-titlebody/{x['source']}_{x.year}_{x.numeric_month}_{x.fourdigitcode}_{make_slug(x.title)}.txt", axis = 1)
 
-for index, row in filesdf.iterrows():
-    f = open(row['outputfilename'], 'w', encoding='utf-8')
-    f.write(row['title'])
-    f.write(row['body'])
-    f.close()
-
-filesdf.to_pickle("../200_data_clean/filesdf.pickle" )
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
+filesdf.to_pickle("../200_data_clean/filesdf.pickle")
--- src/functs.py
+++ src/functs.py
@@ -5,12 +5,15 @@ import spacy
 from spacy.tokens import Span
 import pandas as pd
 from unidecode import unidecode
+import dateparser
+
 
 def obesitylist(*args):
     mylist = ['obesity', 'obese', "obesogenic", "obesogen"]
     for x in args:
         mylist.append(x)
     return mylist
+
 
 def readfilesin(file_path, encoding):
     if encoding in ['ascii', 'Windows-1252', 'ISO-8859-1']:
@@ -29,19 +32,22 @@ def readfilesin(file_path, encoding):
             raise ValueError('Can\'t return dictionary from empty or invalid file %s due to %s' % (file_path, e))
     return data.replace("\r", "").replace("\nClassification\n\n\n", "").strip()
 
+
 def convert_month(month):
-    if month in ["Jan", "Feb", "Mar", "Apr", "May", 
-    "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]:
-        return(datetime.datetime.strptime(month, "%b").strftime("%m"))
-    elif month in ["January", "February", "March", "April", "May", 
-    "June", "July", "August", "September", "October", "November", "December"]:
-        return(datetime.datetime.strptime(month, "%B").strftime("%m"))
+    if month in ["Jan", "Feb", "Mar", "Apr", "May",
+                 "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]:
+        return (datetime.datetime.strptime(month, "%b").strftime("%m"))
+    elif month in ["January", "February", "March", "April", "May",
+                   "June", "July", "August", "September", "October", "November", "December"]:
+        return (datetime.datetime.strptime(month, "%B").strftime("%m"))
     else:
         print("The following month name is not valid: '", month, "'")
+
 
 def where_is_byline(contentslist):
     tmplist = [x.lower().find('byline') for x in contentslist]
     return [i for i, x in enumerate(tmplist) if x != -1]
+
 
 def get_byline(contents):
     byline_withnewline = re.search('Byline: (.*)\n', contents, re.IGNORECASE)
@@ -55,6 +61,7 @@ def get_byline(contents):
     else:
         return None
 
+
 def parse_filename(path):
     source, year, month = path.split('/')[2].split("_")
     month = month.replace("txt", "").strip()
@@ -65,6 +72,7 @@ def parse_filename(path):
         "numeric_month": numeric_month
     })
 
+
 def get_text4digitcode(contents):
     code4digits = re.search(r'(\d+)txt', contents, re.IGNORECASE)
     if code4digits:
@@ -72,18 +80,19 @@ def get_text4digitcode(contents):
     else:
         # some articles from the Age and Brisbane Times have (1) (2) etc numeration instead
         # extract and pad these to four digits to make consistent
-        codeXdigits = re.search(r'\((\d+)\)\.txt', contents,re.IGNORECASE).group(1).zfill(4)
+        codeXdigits = re.search(r'\((\d+)\)\.txt', contents, re.IGNORECASE).group(1).zfill(4)
         return codeXdigits
 
-def clean_nonascii(body, replacementcsvfile = "replacements.csv"):
+
+def clean_nonascii(body, replacementcsvfile="replacements.csv"):
     '''
     Removes non-unicode characters in bodies stored one per row in column of pandas df
-    '''   
+    '''
     # load in replacement dictionary
-    replacementdictionary = {"Â\xad": "' ", "~\xad" : "-", "\\xE2Ä(tm)":"'", "\\xE2Äú":"\"",\
-     "\\xE2Ä\"": "-","\xE2Äò": "\"", "\\xE2€(tm)":"'", "\\xE2€": "'"}
-    replacementdictionary.update(pd.read_csv(replacementcsvfile,quotechar="'", escapechar="\\",\
-    keep_default_na=False).set_index('word')['replacement'].to_dict())
+    replacementdictionary = {"Â\xad": "' ", "~\xad": "-", "\\xE2Ä(tm)": "'", "\\xE2Äú": "\"", \
+                             "\\xE2Ä\"": "-", "\xE2Äò": "\"", "\\xE2€(tm)": "'", "\\xE2€": "'"}
+    replacementdictionary.update(pd.read_csv(replacementcsvfile, quotechar="'", escapechar="\\", \
+                                             keep_default_na=False).set_index('word')['replacement'].to_dict())
     # clean using that
     messybody = body
     bodies_replaced_with_my_dict = mystringreplace(messybody, replacementdictionary)
@@ -93,20 +102,42 @@ def clean_nonascii(body, replacementcsvfile = "replacements.csv"):
     cleaned_bodies = unidecode(bodies_replaced_with_my_dict)
     return cleaned_bodies
 
+
 def clean_quotes(column):
     '''
     Cleans up quotes in body or title
     '''
     mytext = column
     # manually checked 3 quotes are used instead of two in the corpus
-    mytext = mytext.replace('```','"')
+    mytext = mytext.replace('```', '"')
     # two open quotes
-    mytext = mytext.replace('``','"')
+    mytext = mytext.replace('``', '"')
     # two close quotes
-    mytext = mytext.replace("''","\"")
+    mytext = mytext.replace("''", "\"")
     # single quote
-    mytext = mytext.replace("`","'")
+    mytext = mytext.replace("`", "'")
     return mytext
+
+
+def clean_quot(column):
+    '''
+    Deals with &quot; strings in body or metadata
+    '''
+    mytext = column
+    mytext = mytext.replace('&quot;&quot;&quot;', '"')
+    mytext = mytext.replace('&quot;&quot;', '"')
+    mytext = mytext.replace('&quot;', '"')
+    return mytext
+
+
+def replace_six_questionmarks(column):
+    '''
+    Replaces 6 ?????? with a double quote
+    '''
+    # [^?](\?){6}[^?] TODO 
+    mytext = re.sub(r'[^?](\?){6}[^?]', '"', column)
+    return mytext
+
 
 def make_slug(s):
     # Remove all non-word characters (everything except numbers and letters)
@@ -114,8 +145,9 @@ def make_slug(s):
     # Replace all runs of whitespace with a single dash
     s = re.sub(r"\s+", '-', s)
     # truncate ultra-long titles
-    s = s[:200] if len(s) > 200 else s
+    s = s[:150] if len(s) > 150 else s
     return s
+
 
 # to clean the corpus - cleaning symbols in messy bodies
 
@@ -132,29 +164,59 @@ def mystringreplace(string, replacementobject):
         return string
 
 
-def find_problems(start, end, filesdf, colname = "cleaned_bodies"):
+def find_problems(start, end, filesdf, colname="cleaned_bodies"):
     # finding problematic sentences
-    matches = [re.findall(r'\w+.[^\x00-\x7F].+',x)
-    for x in filesdf[colname].iloc[start:end].tolist()]
+    matches = [re.findall(r'\w+.[^\x00-\x7F].+', x)
+               for x in filesdf[colname].iloc[start:end].tolist()]
     return [item
-        for sublist in matches
-         for item in sublist]
+            for sublist in matches
+            for item in sublist]
 
-def find_specific_character_with_preceding(character, start, end, filesdf, colname = "cleaned_bodies"):
+
+def find_specific_character_with_preceding(character, start, end, filesdf, colname="cleaned_bodies"):
     # finding a specific character with the preceding characters
     pattern = r'\w+.' + re.escape(character) + '+.*'
-    return [item for sublist in [re.findall(pattern,x) for x in filesdf[colname].tolist()[start:end]]  for item in sublist]
+    return [item for sublist in [re.findall(pattern, x) for x in filesdf[colname].tolist()[start:end]] for item in
+            sublist]
 
-def find_specific_character_wout_preceding(character, start, end, filesdf, colname = "cleaned_bodies"):
+
+def find_specific_character_wout_preceding(character, start, end, filesdf, colname="cleaned_bodies"):
     # finding a specific character where that character starts a word
     pattern = r'' + re.escape(character) + '+.*'
-    return [item for sublist in [re.findall(pattern,x) for x in filesdf[colname].tolist()[start:end]]  for item in sublist]
+    return [item for sublist in [re.findall(pattern, x) for x in filesdf[colname].tolist()[start:end]] for item in
+            sublist]
+
 
 def find_filename_from_string(string, filesdf):
     return filesdf[filesdf['body'].str.contains(string)]['filename'].to_list()
 
+
 def display_body_from_string(string, filesdf):
     return filesdf[filesdf['body'].str.contains(string)]['body'].to_list()
+
+
+def get_date(string):
+    pattern = re.compile(
+        "(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|"
+        "Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|"
+        "Dec(ember)?)\s+\d{1,2},\s+\d{4}")
+    if pattern.search(string) is not None:
+        return dateparser.parse(pattern.search(string).group())
+    else:
+        return None
+
+
+def write_corpus_titlebody(df, directoryname="corpus-titlebody"):
+    '''
+    Writes our corpus with title and body, without any tags or metadata
+    '''
+    for index, row in filesdf.head().iterrows():
+        outputfilename = f"../200_data_clean/{directoryname}/{row.source}_{row.year}_{row.numeric_month}_{row.fourdigitcode}_{make_slug(row.title)}.txt"
+        f = open(outputfilename, 'w', encoding='utf-8')
+        f.write(row['title'])
+        f.write(row['body'])
+        f.close()
+
 
 # Related to SPACY ------------------
 
@@ -166,13 +228,12 @@ def explore_tokens(sentencenlp_list, obesitynames):
         for token in sentence:
             if token.lemma_ in obesitynames:
                 mydict = {
-                    'text' : token.text,
-                    'tag' : token.tag_,
-                    'dep' : token.dep_,
-                    'head' : token.head,
+                    'text': token.text,
+                    'tag': token.tag_,
+                    'dep': token.dep_,
+                    'head': token.head,
                     'left': token.left_edge,
                     'right': token.right_edge}
                 tokensummarylist.append(mydict)
         sentencesummarylist.append(tokensummarylist)
     return sentencesummarylist
-
