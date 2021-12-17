abdc2a609c6ca22c3ad009004a3bcd9380d7ef7c
Darya <d.vanichkina@gmail.com>
2021-12-17 16:35:20 +1100

    Added all recent work for the last 2 weeks

--- src/003_count_words_in_corpus.py
+++ src/003_count_words_in_corpus.py
@@ -5,11 +5,13 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 from pandas.core.algorithms import diff
-from functs import  get_wordcount_from_metadata, get_record_from_corpus_df, obesitylist, count_words, get_record_by_article_id
+from functs import  get_wordcount_from_metadata, get_record_from_corpus_df, obesitylist, count_words, get_record_by_article_id, sum_all_keywords
 import pathlib
 from utils import get_projectpaths
 (projectroot, rawdatapath, cleandatapath, processeddatapath) = get_projectpaths()
 import seaborn as sns
+from nltk import tokenize
+import re
 
 
 # %%
@@ -19,10 +21,12 @@ corpusdf = pd.read_pickle(processeddatapath/'prepared_corpusdf.pickle')
 # extract length from metadata
 corpusdf = corpusdf.assign(wordcount_from_metatata=corpusdf['metadata'].map(lambda x: get_wordcount_from_metadata(x)))
 # Get a better word count
-# this has been superceded by spacy
 corpusdf['wordcount_body'] = corpusdf['body'].apply(lambda x: count_words(x))
 corpusdf['wordcount_title'] = corpusdf['title'].apply(lambda x: count_words(x))
 corpusdf['wordcount_total'] = corpusdf.loc[:,['wordcount_body','wordcount_title']].sum(axis=1)
+
+
+
 
 
 # %% count how many times words of interest appear in the body and title
@@ -36,9 +40,29 @@ corpusdf['keywords_sum_title'] = corpusdf.loc[:, corpusdf.columns.str.endswith('
 corpusdf['keywords_sum_total'] = corpusdf.keywords_sum_body + corpusdf.keywords_sum_title
 corpusdf['keywords_sum_tara'] = corpusdf["obesity_body_count"] - corpusdf["obesity's_body_count"] + corpusdf["obese_body_count"] + corpusdf["obesity_title_count"] - corpusdf["obesity's_title_count"] + corpusdf["obese_title_count"]
 # boolean as to match the UK corpus keyword count
-corpusdf['obesity_boolean'] = corpusdf['keywords_sum_tara'] >= 3
+corpusdf['obesity_boolean_ukcorpus'] = corpusdf['keywords_sum_tara'] >= 3
 corpusdf['obes_regex_count'] = corpusdf["body"].str.count('[Oo][Bb][Ee][Ss]+\w') + corpusdf["title"].str.count('[Oo][Bb][Ee][Ss]+\w')
 
+# %% first sentence
+corpusdf['first_sent'] = corpusdf['body'].apply(lambda x: tokenize.sent_tokenize(
+    # add a space before period in lowercaseletter.Capital to help NLTK
+    re.sub(r'([a-z])\.([A-Z])', r'\1. \2', 
+    # and digit.Capital , again to help NLTK
+    re.sub(r'([0-9])\.([A-Z])', r'\1. \2', x)
+    )
+    )[0])
+
+corpusdf['first_sent_count'] = corpusdf['first_sent'].apply(
+    lambda x: sum_all_keywords(
+        x, 
+        # this makes sure that obesity's (and other plurals) is replaced with obesity and counted only once
+        set(map(lambda x: x.replace("'s", ""), obesitylist()))
+        ))
+
+corpusdf['obesity_header_count'] = corpusdf['keywords_sum_title'] + corpusdf['first_sent_count']
+corpusdf['obesity_boolean_header'] = corpusdf['obesity_header_count'] > 0
+
+# %%
 differing_counts = corpusdf[corpusdf['obes_regex_count'] != corpusdf['keywords_sum_total']]
 differing_counts.to_csv(projectroot/processeddatapath/'differing_counts.csv', index = False)
 
@@ -71,5 +95,5 @@ fig.show()
 
 
 # %% export cleaned corpus with word counts
-# note no filtering has happened in this file, though we may want to later remove some crossword articles etc
+# note no filtering has happened in this file
 pd.to_pickle(corpusdf, processeddatapath/'corpusdf_with_wc.pickle')
---
+++ src/007_export_corpora.py
@@ -0,0 +1,80 @@
+# %%
+import re
+import shutil
+from datetime import datetime
+import numpy as np
+import pandas as pd
+from functs import write_corpus_summary_tables, get_record_by_article_id
+
+from functs import write_corpus_titlebody, write_corpus_sketchengine,  write_corpus_cqpweb, write_corpus_nested
+import pathlib
+from utils import get_projectpaths
+(projectroot, rawdatapath, cleandatapath, processeddatapath) = get_projectpaths()
+
+# %%
+corpusdf = pd.read_pickle(processeddatapath/'corpusdf_with_topics.pickle')
+
+# %%
+# generate a corpus with 3+ counts of obesity, as per tara
+corpusdf_tara = corpusdf.copy()
+corpusdf_tara = corpusdf_tara[corpusdf_tara['obesity_boolean_ukcorpus'] == True].reset_index(drop = True)
+
+# %%
+# generate a corpus with 1+ count of obesity in the title or first sentence
+corpusdf_header = corpusdf.copy()
+corpusdf_header = corpusdf_header[corpusdf_header['obesity_boolean_header'] == True].reset_index(drop = True)
+
+# %% write full corpus out as per client's request
+write_corpus_titlebody(df=corpusdf, cleandatapath=cleandatapath, directoryname="corpus_titlebody")
+write_corpus_cqpweb(inputdf=corpusdf, cleandatapath=cleandatapath, directoryname="corpus_cqpweb", write_actual_files=True)
+write_corpus_sketchengine(inputdf=corpusdf, cleandatapath=cleandatapath, directoryname="corpus_sketchengine")
+write_corpus_nested(df=corpusdf, cleandatapath=cleandatapath, directoryname="corpus_nested")
+
+# %% 3+ mentions
+write_corpus_titlebody(df=corpusdf_tara, cleandatapath=cleandatapath, directoryname="corpus_titlebody_3plus")
+write_corpus_sketchengine(inputdf=corpusdf_tara, cleandatapath=cleandatapath, directoryname="corpus_sketchengine_3plus")
+write_corpus_nested(df=corpusdf_tara, cleandatapath=cleandatapath, directoryname="corpus_nested_3plus")
+write_corpus_cqpweb(inputdf=corpusdf, cleandatapath=cleandatapath, directoryname="corpus_3plus", write_actual_files=False)
+
+
+# %% in header (1 sentence & title)
+write_corpus_titlebody(df=corpusdf_header, cleandatapath=cleandatapath, directoryname="corpus_titlebody_header")
+write_corpus_sketchengine(inputdf=corpusdf_header, cleandatapath=cleandatapath, directoryname="corpus_sketchengine_header")
+write_corpus_nested(df=corpusdf_header, cleandatapath=cleandatapath, directoryname="corpus_nested_header")
+write_corpus_cqpweb(inputdf=corpusdf, cleandatapath=cleandatapath, directoryname="corpus_header", write_actual_files=False)
+
+# %% write for testing
+#corpusdf_mini = corpusdf.groupby('source').head(15).reset_index(drop=True)
+#write_corpus_cqpweb(inputdf=corpusdf_mini, cleandatapath=cleandatapath, directoryname="corpus_cqpweb-mini")
+
+
+
+
+# just including articles with three or more mentions in article (either body or headline)
+write_corpus_summary_tables(
+    corpusdf=corpusdf, 
+    cleandatapath=cleandatapath, 
+    articlecounts_name="articlecounts_full", 
+    wordcounts_name="wordcounts_full")
+# same for tara corpus version
+write_corpus_summary_tables(
+    corpusdf=corpusdf_tara, 
+    cleandatapath=cleandatapath, 
+    articlecounts_name="articlecounts_3plus", 
+    wordcounts_name="wordcounts_3plus")
+# and for header corpus version
+write_corpus_summary_tables(
+    corpusdf=corpusdf_header, 
+    cleandatapath=cleandatapath, 
+    articlecounts_name="articlecounts_header", 
+    wordcounts_name="wordcounts_header")
+
+# %% export in data science formats
+#corpusdf.to_pickle(cleandatapath/'corpusdf_final.pickle')
+#corpusdf.to_csv(cleandatapath/"corpusdf_final.csv", index=False)
+
+# %% copy to cloudstor
+# very fragile local link :(
+# shutil.copytree(cleandatapath, '../../../../cloudstor/projects/obesity', dirs_exist_ok=True)
+
+
